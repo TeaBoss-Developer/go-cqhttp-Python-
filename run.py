f7cdb0fa6fb7ae4650bfe7a50089435d45dabe3e
TeaBoss-Developer <104977213+TeaBoss-Developer@users.noreply.github.com>
2022-06-18 23:07:44 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ import os
 import random
 import requests
 se = requests.session()
+
 #初始化事件接收器
 def InitSocket(address,port):
     Other_time("绑定地址");
@@ -41,51 +42,29 @@ def StartListening(sock,address,port):
         txx.close()
         try:
             if "\"message_type\":\"group" in buf.decode('utf-8','ignore'):#监听群消息
-                qhz = g[0].split("\"group_id\":")
-                qhy = qhz[1].split(",\"message\"")
-                SQz = g[0].split("\"user_id\":")
-                sq = SQz[1].split("},\"sub")
-                idz = g[0].split("message_id\":")
-                _id = idz[1].split(",\"message")
-                sfz = g[0].split("\"self_id\":")
-                sf = sfz[1].split(",\"sender\"")
-                nr = g[0].split("\"message\":\"")
-                nrz = nr[1].split("\",\"message_id\":")
-                print("收到群聊消息\n"+"群号:"+qhy[0]+"\n发送人QQ号:"+sq[0]+"\n消息内容:"+nrz[0]+"\n框架QQ:"+sf[0]+"\n消息ID:"+_id[0])
+                Gm = Group_msg(g[0])
+                print("收到群聊消息\n"+"群号:"+Gm.group_id+"\n发送人QQ号:"+Gm.sender+"\n消息内容:"+Gm.msg+"\n框架QQ:"+Gm.self_id+"\n消息ID:"+Gm.msg_id)
                 for myg in syt:
                     info = myg.split(" ")
                     if(info[1] == "精确"):
-                        if(info[0] == nrz[0]):
+                        if(info[0] == Gm.msg):
                             if("tcq:img" in myg):
                                 img = info[2].split("info=")
                                 imgu = img[1].split("]")
-                                Post_url = "http://127.0.0.1:5700/send_group_msg?group_id="+qhy[0]+"&message=[CQ:image,file="+imgu[0]+",id=40000]"
-                                Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
-                                print(Text)
+                                Api.send_group_msg(Gm.group_id,Texts.image(imgu[0]))
                             if("tcq:img" not in myg):
-                                Post_url = "http://127.0.0.1:5700/send_group_msg?group_id="+qhy[0]+"&message="+info[2]
-                                Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
-                                print(Text)
+                                Api.send_private_msg(Gm.group_id,info[2])
                     if(info[1] == "模糊"):
-                        if(info[0] in nrz[0]):
-                            Post_url = "http://127.0.0.1:5700/send_group_msg?group_id="+qhy[0]+"&message="+info[2]
-                            Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
-                            print(Text)
-                if("CQ:image" in nrz[0] and "type=flash" in nrz[0]):
-                    Api.send_private_msg("3302347619",qhy[0]+"里"+sq[0]+"发的闪照破解完啦"+nrz[0].replace("flash",""))
-                    Api.send_private_msg("2220067959",qhy[0]+"里"+sq[0]+"发的闪照破解完啦"+nrz[0].replace("flash",""))
+                        if(info[0] in Gm.msg):
+                            Api.send_group_msg(Gm.group_id,info[2])
+                if("CQ:image" in Gm.msg and "type=flash" in Gm.msg):
+                    Api.send_private_msg("3302347619",Gm.group_id+"里"+Gm.sender+"发的闪照破解完啦"+Group_msg.msg.replace("flash",""))
+                    Api.send_private_msg("2220067959",Gm.group_id+"里"+Gm.sender+"发的闪照破解完啦"+Group_msg.msg.replace("flash",""))
             if "\"message_type\":\"private" in buf.decode('utf-8','ignore'):#监听私聊消息
-                SQz = g[0].split("\"user_id\":")
-                sq = SQz[1].split("},\"sub")
-                idz = g[0].split("message_id\":")
-                _id = idz[1].split(",\"message")
-                sfz = g[0].split("\"self_id\":")
-                sf = sfz[1].split(",\"sender\"")
-                nr = g[0].split("\"message\":\"")
-                nrz = nr[1].split("\",\"message_id\":")
-                print("收到私聊消息\n"+"发送人QQ号:"+sq[0]+"\n消息内容:"+nrz[0]+"\n框架QQ:"+sf[0]+"\n消息ID:"+_id[0])
-                if("新建词汇" in nrz[0]):
-                    lists = nrz[0].split(" ")#[1]模式(精确/模糊)[2]关键词[3]返回内容
+                Pm = Private_msg(g[0])
+                print("收到私聊消息\n"+"发送人QQ号:"+Pm.sender+"\n消息内容:"+Pm.msg+"\n框架QQ:"+Pm.self_id+"\n消息ID:"+Pm.msg_id)
+                if("新建词汇" in Pm.msg):
+                    lists = Pm.msg.split(" ")#[1]模式(精确/模糊)[2]关键词[3]返回内容
                     readcode = open("code.txt",'r')
                     text = readcode.read()
                     readcode.close()
@@ -93,46 +72,25 @@ def StartListening(sock,address,port):
                     save=open("code.txt",'w')
                     save.write(buf1)
                     save.close()
-                    Api.send_private_msg(sq[0],Texts.face("30")+"添加词汇成功,请尝试触发.")
+                    Api.send_private_msg(Pm.sender,Texts.face("30")+"添加词汇成功,请尝试触发.")
                     
             if("\"notice_type\":\"group_recall\"" in g[0]):#监听撤回消息
-                qhz = g[0].split("\"group_id\":")
-                qhy = qhz[1].split(",\"message_id\":")
-                SQz = g[0].split("\"user_id\":")
-                sq = SQz[1].split("}")
-                xxz = g[0].split("\"message_id\":")
-                xxm = xxz[1].split(",\"notice_type\"")
-                print("收到消息撤回事件\n群号为:"+qhy[0]+"\n发送人QQ:"+sq[0]+"\n消息ID为(用来查询消息内容):"+xxm[0])
+                GR = Group_Recall(g[0])
+                print("收到消息撤回事件\n群号为:"+GR.group_id+"\n发送人QQ:"+GR.sender+"\n消息ID为(用来查询消息内容):"+GR.msg_id)
             if("group_ban" in g[0]):#监听禁言消息
-                qhz = g[0].split("\"group_id\":")
-                qhy = qhz[1].split(",\"notice_type\"")
-                sjz = g[0].split("\"duration\":")
-                sjy = sjz[1].split(",\"group_id")
-                czz = g[0].split("\"operator_id\":")
-                czy = czz[1].split(",\"post_type")
-                bez = g[0].split("\"user_id\":")
-                bey = bez[1].split("}")
-                if(sjy[0] != "0"):
-                    print("收到禁言事件\n群号为:"+qhy[0]+"\n管理QQ:"+czy[0]+"\n封禁时间:"+sjy[0]+"\n被禁言者QQ:"+bey[0])
-                    Post_url = "http://127.0.0.1:5700/send_group_msg?group_id="+qhy[0]+"&message="+bey[0]+"被"+czy[0]+"禁言了。时间是:"+sjy[0]+"秒"
-                    Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
-                    print(Text)
-                if(sjy[0] == "0"):
-                    print("收到解禁言事件\n群号为:"+qhy[0]+"\n管理QQ:"+czy[0]+"\n解开禁言者QQ:"+bey[0])
-                    Post_url = "http://127.0.0.1:5700/send_group_msg?group_id="+qhy[0]+"&message="+bey[0]+"被解开禁言了。"
-                    Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
-                    print(Text)
+                GB = Group_ban(g[0])
+                if(GB.time != "0"):
+                    print("收到禁言事件\n群号为:"+GB.group_id+"\n管理QQ:"+GB.operator_id+"\n封禁时间:"+GB.time+"\n被禁言者QQ:"+GB.user_id)
+                    Api.send_group_msg(GB.group_id,GB.user_id+"被"+GB.operator_id+"禁言了。时间是:"+GB.time+"秒")
+                if(GB.time == "0"):
+                    print("收到解禁言事件\n群号为:"+GB.group_id+"\n管理QQ:"+GB.operator_id+"\n解开禁言者QQ:"+GB.user_id)
+                    Api.send_group_msg(GB.group_id,GB.user_id+"被"+GB.operator_id+"解开禁言了。")
             if("group_admin" in g[0]):#监听设置管理员事件
-                qhz = g[0].split("\"group_id\":")
-                qhy = qhz[1].split(",\"notice_type\"")
-                setz = g[0].split("\"sub_type\":\"")
-                sety = qhz[1].split("\",\"time\"")
-                bez = g[0].split("\"user_id\":")
-                bey = bez[1].split("}")
-                if(sety[0] == "set"):
-                    print("收到设置管理事件\n群号:"+qhy[0]+"被设置人员:"+bey[0])
-                if(sety[0] == "unset"):
-                    print("收到收回管理事件\n群号:"+qhy[0]+"被设置人员:"+bey[0])
+                GA = Group_Admin(g[0])
+                if(GA.sub_type == "set"):
+                    print("收到设置管理事件\n群号:"+GA.group_id+"被设置人员:"+GA.user_id)
+                if(GA.sub_type == "unset"):
+                    print("收到收回管理事件\n群号:"+GA.group_id+"被设置人员:"+GA.user_id)
             if("\"message_type\":\"private" not in buf.decode('utf-8','ignore') and "\"message_type\":\"group" not in buf.decode('utf-8','ignore')and "\"notice_type\":\"group_recall\"" not in g[0] and "group_ban" not in g[0]):
                 print("未知事件JSON为:"+g[0])
         except:
@@ -207,7 +165,7 @@ class Api:#API类(SA:好友消息,群消息.)
         Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
         print(Text)
     def set_group_special_title(group_id,uesr_id,special_title):
-        Post_url = "http://127.0.0.1:5700/set_group_special_title?group_id="+group_id+"&user_id="+user_id+"&special_title="+special_title
+        Post_url = "http://127.0.0.1:5700/set_group_special_title?group_id="+group_id+"&user_id="+uesr_id+"&special_title="+special_title
         Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
         print(Text)
     def set_friend_add_request():
@@ -259,7 +217,7 @@ class Api:#API类(SA:好友消息,群消息.)
         Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
         print(Text)
     def _set_model_show(modle,modle_show):
-        Post_url = "http://127.0.0.1:5700/_set_model_show?model="+model+"&modle_show="+modle_show
+        Post_url = "http://127.0.0.1:5700/_set_model_show?model="+modle+"&modle_show="+modle_show
         Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
         print("具体机型在这个链接里查看:https://github.com/Mrs4s/go-cqhttp/pull/872#issuecomment-831180149")
     def _send_group_notice(group_id,content,image_path):
@@ -276,13 +234,94 @@ class Api:#API类(SA:好友消息,群消息.)
         Post_url = "http://127.0.0.1:5700/set_group_portrait?group_id="+group_id+"&file="+file+"&cache="+cache
         Text = se.post(Post_url).text.replace("'", '"').replace('/ ', '/')
         print("具体操作file可以为什么数据请看https://docs.go-cqhttp.org/api/#%E8%AE%BE%E7%BD%AE%E7%BE%A4%E5%A4%B4%E5%83%8F")
-    
+class Private_msg:
+    sender = ""
+    self_id = ""
+    msg_id = ""
+    msg = ""
+    def __init__(self,msg1):
+        SQz = msg1.split("\"user_id\":")
+        sq = SQz[1].split("},\"sub")
+        idz = msg1.split("message_id\":")
+        _id = idz[1].split(",\"message")
+        sfz = msg1.split("\"self_id\":")
+        sf = sfz[1].split(",\"sender\"")
+        nr = msg1.split("\"message\":\"")
+        nrz = nr[1].split("\",\"message_id\":")
+        self.msg = nrz[0]
+        self.self_id = sf[0]
+        self.sender = sq[0]
+        self.msg_id = _id[0]
+class Group_Recall:
+    group_id = ""
+    sender = ""
+    msg_id = ""
+    def __init__(self,msg1):
+        qhz = msg1.split("\"group_id\":")
+        qhy = qhz[1].split(",\"message_id\":")
+        SQz = msg1.split("\"user_id\":")
+        sq = SQz[1].split("}")
+        xxz = msg1.split("\"message_id\":")
+        xxm = xxz[1].split(",\"notice_type\"")
+        self.group_id=qhy[0]
+        self.msg_id=xxm[0]
+        self.sender=sq[0]
+class Group_msg:
+    group_id = ""
+    self_id = ""
+    sender = ""
+    msg_id = ""
+    msg = ""
+    def __init__(self,msg1):
+        qhz = msg1.split("\"group_id\":")
+        qhy = qhz[1].split(",\"message\"")
+        SQz = msg1.split("\"user_id\":")
+        sq = SQz[1].split("},\"sub")
+        idz = msg1.split("message_id\":")
+        _id = idz[1].split(",\"message")
+        sfz = msg1.split("\"self_id\":")
+        sf = sfz[1].split(",\"sender\"")
+        nr = msg1.split("\"message\":\"")
+        nrz = nr[1].split("\",\"message_id\":")
+        self.group_id = qhy[0]
+        self.self_id = sf[0]
+        self.sender = sq[0]
+        self.msg_id = _id[0]
+        self.msg = nrz[0]
+class Group_ban:
+    group_id = ""
+    time = ""
+    operator_id = ""
+    user_id = ""
+    def __init__(self,msg1):
+        qhz = msg1.split("\"group_id\":")
+        qhy = qhz[1].split(",\"notice_type\"")
+        sjz = msg1.split("\"duration\":")
+        sjy = sjz[1].split(",\"group_id")
+        czz = msg1.split("\"operator_id\":")
+        czy = czz[1].split(",\"post_type")
+        bez = msg1.split("\"user_id\":")
+        bey = bez[1].split("}")
+        self.group_id = qhy[0]
+        self.operator_id = czy[0]
+        self.time = sjy[0]
+        self.user_id = bey[0]
+class Group_Admin:
+    group_id = ""
+    sub_type = ""
+    user_id = ""
+    def __init__(self,msg1):
+        qhz = msg1.split("\"group_id\":")
+        qhy = qhz[1].split(",\"notice_type\"")
+        setz = msg1.split("\"sub_type\":\"")
+        sety = setz[1].split("\",\"time\"")
+        bez = msg1.split("\"user_id\":")
+        bey = bez[1].split("}")
+        self.group_id = qhy[0]
+        self.sub_type = sety[0]
+        self.user_id = bey[0]
 def CloseListening(sock):#关闭事件监听器
     sock.close();
-    
-#CmdSolve
-
-
 def Other_time(behavior):#进行监听事件报告
     global Address;
     global Port;
@@ -295,7 +334,6 @@ def main():
     address=Address;
     port=Port
     sock=InitSocket(address,port);
-
     StartListening(sock,address,port);
     CloseListening(sock);
     return 0;
